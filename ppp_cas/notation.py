f83a25391f84c996887ffe4a62758a2bd7d8c46e
Sir Isaac Newton <marc.chevalier@ens-lyon.org>
2014-11-22 22:52:22 +0100

    Extends the list of mathematical symbols

--- ppp_cas/notation.py
+++ ppp_cas/notation.py
@@ -1,5 +1,5 @@
 def isMath(formula):
-    caracteristicSymbols = ['\\', '/', '*', '-', '(', ')', '_', '^', '+', '[', ']', '{', '}', '=']
+    caracteristicSymbols = ['\\', '/', '*', '-', '(', ')', '_', '^', '+', '[', ']', '{', '}', '=', 'sqrt', '0', '1', '2', '3','4', '5','6', '7','8', '9']
     for e in caracteristicSymbols:
         if e in formula:
             return True
