a4276518946cba8a4b9b30addaba69e8b37f55f7
Valentin Lorentz <progval@progval.net>
2014-11-29 21:57:30 +0100

    More pythonic (+ efficient?) code for notation.isMath.

--- ppp_cas/notation.py
+++ ppp_cas/notation.py
@@ -1,9 +1,6 @@
+CARACTERISTIC_SYMBOLS = ['sqrt'] + list(r'\/*-()_^+[]{}=!%0123456789')
 def isMath(formula):
-    caracteristicSymbols = ['\\', '/', '*', '-', '(', ')', '_', '^', '+', '[', ']', '{', '}', '=', '!', '%', 'sqrt', '0', '1', '2', '3','4', '5','6', '7','8', '9']
-    for e in caracteristicSymbols:
-        if e in formula:
-            return True
-    return False
+    return any(e in formula for e in CARACTERISTIC_SYMBOLS)
 
 def relevance(inputFormula, outputTree):
     lenOutput = len(str(outputTree))
