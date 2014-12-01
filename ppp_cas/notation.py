c87736262f43cc6e9407e2680960524a08c9c48f
Valentin Lorentz <progval@progval.net>
2014-12-01 18:28:15 +0100

    Revert "Bug fix."
    This reverts commit 6f10377ad744fc7ef714e3a1c85a94d017edd6d6.

--- ppp_cas/notation.py
+++ ppp_cas/notation.py
@@ -3,7 +3,7 @@ def isMath(formula):
     return any(e in formula for e in CARACTERISTIC_SYMBOLS)
 
 def traceContainsSpellChecker(trace):
-    return any(e['module'] == 'spell-checker' for e in trace)
+    return any(e.module == 'spell-checker' for e in trace)
 
 def relevance(inputFormula, outputTree):
     lenOutput = len(str(outputTree))
