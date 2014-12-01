b4a8649c7968870c116269b251b417fe4d942a55
Valentin Lorentz <progval@progval.net>
2014-12-01 18:30:50 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppp_cas',
-    version='0.3',
+    version='0.3.1',
     description='CAS plugin for PPP',
     url='https://github.com/ProjetPP',
     author='Projet Pensées Profondes',
@@ -24,7 +24,7 @@ setup(
         'Topic :: Software Development :: Libraries',
     ],
     install_requires=[
-        'ppp_datamodel>=0.5',
+        'ppp_datamodel>=0.5.10',
         'ppp_libmodule>=0.7,<0.8',
     ],
     packages=[
